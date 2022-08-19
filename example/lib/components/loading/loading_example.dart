84399d71a9e75dd0d093d50311838be3821ac5d9
yixiaco <627428179@qq.com>
2022-08-19 14:23:08 +0800

    完成TLoading组件

--- example/lib/components/loading/loading_example.dart
+++ example/lib/components/loading/loading_example.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:tdesign_desktop_ui/tdesign_desktop_ui.dart';
 
@@ -9,14 +11,52 @@ class TLoadingExample extends StatefulWidget {
 }
 
 class _TLoadingExampleState extends State<TLoadingExample> {
+  bool loading = true;
+
   @override
   Widget build(BuildContext context) {
-    return const TSpace(
+    return TSpace(
       direction: Axis.vertical,
       children: [
-        LinearProgressIndicator(),
-        CircularProgressIndicator(),
-        TLoading(),
+        const TLoading(
+          text: Text('加载中...'),
+        ),
+        TLoading(
+          loading: loading,
+          text: const Text('加载中...'),
+          child: const Text('this is loading component\n'
+              'this is loading component\n'
+              'this is loading component\n'
+              'this is loading component\n'
+              'this is loading component'),
+        ),
+        TLoading(
+          loading: loading,
+          delay: const Duration(milliseconds: 500),
+          child: const Text('this is loading component'),
+        ),
+        TRadioGroup<bool>(
+          variant: TRadioVariant.defaultFilled,
+          options: [
+            TRadioOption(label: const Text('加载中'), value: true),
+            TRadioOption(label: const Text('加载完成'), value: false),
+          ],
+          value: loading,
+          onChange: (value) {
+            setState(() {
+              loading = value!;
+            });
+          },
+        ),
+        TButton(
+          child: const Text('全屏加载（开启加载2秒后自动归位）'),
+          onPressed: () {
+            TLoading.showFullLoading(context: context);
+            Timer(const Duration(seconds: 2), () {
+              Navigator.pop(context);
+            });
+          },
+        ),
       ],
     );
   }
