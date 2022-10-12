49797b31afbe307991d93bae7fed706bae6ff387
yixiaco <627428179@qq.com>
2022-10-12 10:16:43 +0800

    格式化

--- lib/src/util/validate/is_number.dart
+++ lib/src/util/validate/is_number.dart
@@ -1,10 +1,10 @@
 /// 验证是否是一个数字
 bool isNumber(input) {
-  if(input is num) {
+  if (input is num) {
     return true;
   }
   var str = input?.toString();
-  if(str != null) {
+  if (str != null) {
     return num.tryParse(str) != null;
   }
   return false;
