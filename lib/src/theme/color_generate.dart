984ef06febf9398d6bd38b11482ec3fd9645ff6a
yixiaco <co627428179@gmail.com>
2023-06-16 01:59:01 +0800

    perf: 重构主题颜色生成代码方法

--- lib/src/theme/color_generate.dart
+++ lib/src/theme/color_generate.dart
@@ -3,15 +3,16 @@ import 'dart:core';
 import 'package:dartx/dartx.dart';
 import 'package:flutter/painting.dart';
 
-const hueStep = 2; // 色相阶梯
-const saturationStep = 0.16; // 饱和度阶梯，浅色部分
-const saturationStep2 = 0.05; // 饱和度阶梯，深色部分
-const brightnessStep1 = 0.05; // 亮度阶梯，浅色部分
-const brightnessStep2 = 0.15; // 亮度阶梯，深色部分
-const lightColorCount = 5; // 浅色数量，主色上
-const darkColorCount = 4; // 深色数量，主色下
+const _hueStep = 2; // 色相阶梯
+const _saturationStep = 0.16; // 饱和度阶梯，浅色部分
+const _saturationStep2 = 0.05; // 饱和度阶梯，深色部分
+const _brightnessStep1 = 0.05; // 亮度阶梯，浅色部分
+const _brightnessStep2 = 0.15; // 亮度阶梯，深色部分
+const _lightColorCount = 5; // 浅色数量，主色上
+const _darkColorCount = 4; // 深色数量，主色下
+const _defaultBackgroundColor = '#141414';
 // 暗色主题颜色映射关系表
-const darkColorMap = [
+const _darkColorMap = [
   (index: 7, opacity: 0.15),
   (index: 6, opacity: 0.25),
   (index: 5, opacity: 0.3),
@@ -24,23 +25,18 @@ const darkColorMap = [
   (index: 1, opacity: 0.98),
 ];
 
-class HsvObject {
-  final double h;
-  final double s;
-  final double v;
-
-  HsvObject({required this.h, required this.s, required this.v});
-}
-
+/// RGB转为HSV
 HSVColor rgbToHsv(int r, int g, int b) {
   return HSVColor.fromColor(Color.fromRGBO(r, g, b, 1));
 }
 
-String rgbToHex(int r, int g, int b) {
-  var hex = Color.fromRGBO(r, g, b, 1).value.toRadixString(16).padLeft(8, '0');
-  return hex.substring(2) + hex.substring(0,2);
+/// RGB转为HEX，遵循CSS样式风格
+String rgbToHex(Color color) {
+  var hex = color.value.toRadixString(16).padLeft(8, '0');
+  return hex.substring(2) + hex.substring(0, 2);
 }
 
+/// HEX转为Color，输入遵循CSS风格
 Color inputToRGB(String color) {
   var colorHex = color.removePrefix('#');
   var hex = colorHex.padRight(8, 'F');
@@ -49,19 +45,15 @@ Color inputToRGB(String color) {
   return Color(int.parse(hex, radix: 16));
 }
 
+/// HSV转为Color
 Color hsvToRGB({required double h, required double s, required double v}) {
   return HSVColor.fromAHSV(1, h, s, v).toColor();
-}
-
-HsvObject toHsv({required int r, required int g, required int b}) {
-  final hsv = rgbToHsv(r, g, b);
-  return HsvObject(h: hsv.hue, s: hsv.saturation, v: hsv.value);
 }
 
 /// Wrapper function ported from TinyColor.prototype.toHexString
 /// Keep it here because of the prefix `#`
 String toHex(Color color) {
-  return '#${rgbToHex(color.red, color.green, color.blue)}';
+  return '#${rgbToHex(color)}';
 }
 
 /// Wrapper function ported from TinyColor.prototype.mix, not treeshakable.
@@ -78,13 +70,13 @@ Color mix(Color rgb1, Color rgb2, double amount) {
   return rgb;
 }
 
-double getHue(HsvObject hsv, int i, [light = false]) {
+double _getHue(HSVColor hsv, int i, [light = false]) {
   double hue;
   // 根据色相不同，色相转向不同
-  if (hsv.h.round() >= 60 && hsv.h.round() <= 240) {
-    hue = (light ? hsv.h.round() - hueStep * i : hsv.h.round() + hueStep * i).toDouble();
+  if (hsv.hue.round() >= 60 && hsv.hue.round() <= 240) {
+    hue = (light ? hsv.hue.round() - _hueStep * i : hsv.hue.round() + _hueStep * i).toDouble();
   } else {
-    hue = (light ? hsv.h.round() + hueStep * i : hsv.h.round() - hueStep * i).toDouble();
+    hue = (light ? hsv.hue.round() + _hueStep * i : hsv.hue.round() - _hueStep * i).toDouble();
   }
   if (hue < 0) {
     hue += 360;
@@ -94,25 +86,25 @@ double getHue(HsvObject hsv, int i, [light = false]) {
   return hue;
 }
 
-double getSaturation(HsvObject hsv, int i, [light = false]) {
+double _getSaturation(HSVColor hsv, int i, [light = false]) {
   // grey color don't change saturation
-  if (hsv.h == 0 && hsv.s == 0) {
-    return hsv.s;
+  if (hsv.hue == 0 && hsv.saturation == 0) {
+    return hsv.saturation;
   }
   double saturation;
   if (light) {
-    saturation = hsv.s - saturationStep * i;
-  } else if (i == darkColorCount) {
-    saturation = hsv.s + saturationStep;
+    saturation = hsv.saturation - _saturationStep * i;
+  } else if (i == _darkColorCount) {
+    saturation = hsv.saturation + _saturationStep;
   } else {
-    saturation = hsv.s + saturationStep2 * i;
+    saturation = hsv.saturation + _saturationStep2 * i;
   }
   // 边界值修正
   if (saturation > 1) {
     saturation = 1;
   }
   // 第一格的 s 限制在 0.06-0.1 之间
-  if (light && i == lightColorCount && saturation > 0.1) {
+  if (light && i == _lightColorCount && saturation > 0.1) {
     saturation = 0.1;
   }
   if (saturation < 0.06) {
@@ -121,12 +113,12 @@ double getSaturation(HsvObject hsv, int i, [light = false]) {
   return double.parse(saturation.toStringAsFixed(2));
 }
 
-double getValue(HsvObject hsv, int i, [light = false]) {
+double _getValue(HSVColor hsv, int i, [light = false]) {
   double value;
   if (light) {
-    value = hsv.v + brightnessStep1 * i;
+    value = hsv.value + _brightnessStep1 * i;
   } else {
-    value = hsv.v - brightnessStep2 * i;
+    value = hsv.value - _brightnessStep2 * i;
   }
   if (value > 1) {
     value = 1;
@@ -134,40 +126,42 @@ double getValue(HsvObject hsv, int i, [light = false]) {
   return double.parse(value.toStringAsFixed(2));
 }
 
-List<String> generate(String color, {String? theme, String? backgroundColor}) {
-  var patterns = <String>[];
-  final pColor = inputToRGB(color);
-  for (var i = lightColorCount; i > 0; i -= 1) {
-    final hsv = toHsv(r: pColor.red, g: pColor.green, b: pColor.blue);
+List<String> generateOfCssColor(String color, {bool light = true, String? backgroundColor}) {
+  var bg = backgroundColor != null ? inputToRGB(backgroundColor) : null;
+  var generateColors = generate(inputToRGB(color), light: light, backgroundColor: bg);
+  return generateColors.map((e) => toHex(e)).toList();
+}
+
+List<Color> generate(Color color, {bool light = true, Color? backgroundColor}) {
+  var patterns = <Color>[];
+  for (var i = _lightColorCount; i > 0; i -= 1) {
+    final hsv = rgbToHsv(color.red, color.green, color.blue);
     var rgb = hsvToRGB(
-      h: getHue(hsv, i, true),
-      s: getSaturation(hsv, i, true),
-      v: getValue(hsv, i, true),
+      h: _getHue(hsv, i, true),
+      s: _getSaturation(hsv, i, true),
+      v: _getValue(hsv, i, true),
     );
-    final colorString = toHex(rgb);
-    patterns.add(colorString);
+    patterns.add(rgb);
   }
-  patterns.add(toHex(pColor));
-  for (var i = 1; i <= darkColorCount; i += 1) {
-    final hsv = toHsv(r: pColor.red, g: pColor.green, b: pColor.blue);
+  patterns.add(color);
+  for (var i = 1; i <= _darkColorCount; i += 1) {
+    final hsv = rgbToHsv(color.red, color.green, color.blue);
     var rgb = hsvToRGB(
-      h: getHue(hsv, i),
-      s: getSaturation(hsv, i),
-      v: getValue(hsv, i),
+      h: _getHue(hsv, i),
+      s: _getSaturation(hsv, i),
+      v: _getValue(hsv, i),
     );
-    final colorString = toHex(rgb);
-    patterns.add(colorString);
+    patterns.add(rgb);
   }
 
   // dark theme patterns
-  if (theme == 'dark') {
-    return darkColorMap.map((e) {
-      var mix2 = mix(
-        inputToRGB(backgroundColor ?? '#141414'),
-        inputToRGB(patterns[e.index]),
+  if (!light) {
+    return _darkColorMap.map((e) {
+      return mix(
+        backgroundColor ?? inputToRGB(_defaultBackgroundColor),
+        patterns[e.index],
         e.opacity * 100,
       );
-      return toHex(mix2);
     }).toList();
   }
   return patterns;
